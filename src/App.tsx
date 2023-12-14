cac82a7ceea3b6a4a0a9650ce2f383b68d660a59
Swazi Kunene <kuneneswazi18@gmail.com>
2023-12-14 19:50:10 +0200

    responsive on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { ListComponent } from './components/ListComponent';
 import ReactPaginate from 'react-paginate';
@@ -13,8 +13,8 @@ const App: React.FC = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<String>('All');
   const [filteredList, setFilteredList] = useState<TimelineItem[] | []>([]);
-
   const [itemsPerPage, setItemsPerPage] = useState(24);
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
 
   const fetchTimeline = () => {
     let xhr = new XMLHttpRequest();
@@ -71,7 +71,12 @@ const App: React.FC = () => {
     <div className="app">
       <div className="app__wrapper">
         <div className="app__top">
-          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="logo" />
+          <img
+            src={`${process.env.PUBLIC_URL}/logo.png`}
+            alt="logo"
+            className='app__logo'
+          />
+
           <h1 className="app__title">Dr. Arthur Frost</h1>
         </div>
 
@@ -79,7 +84,65 @@ const App: React.FC = () => {
 
         <h2 className="app__heading">Dr Arthur's Timeline</h2>
 
-        {loading
+        <div className="app__container">
+          <div className='app__filters'>
+              <select
+                name="items-per-page"
+                id="items-per-page"
+                defaultValue={"0"}
+                onChange={(event) => {
+                  setItemsPerPage(Number(event.target.value))
+                }}
+                className="app__filters-select"
+              >
+                <option value="0" disabled>Select the number of items per page</option>
+                <option value="12">12</option>
+                <option value="24">24</option>
+                <option value="36">36</option>
+                <option value="48">48</option>
+              </select>
+
+              <select
+                name="categories"
+                id="categories"
+                defaultValue={"0"}
+                onChange={(event) => {
+                  setSelectedCategory(event.target.value)
+                }}
+                className="app__filters-select"
+              >
+                <option value="0" disabled>Select Category</option>
+                <option value="All">All</option>
+                {categories.map((category) => (
+                  <option value={category}>{category}</option>
+                ))}
+              </select>
+          </div>
+
+          {loading
+            ? (<Loader />)
+            : (<ListComponent timelineList={currentItems} />)
+          }
+
+          <ReactPaginate
+            containerClassName={"pagination"}
+            activeClassName={"item active"}
+            disabledClassName={"disabled-page"}
+            breakLabel="..."
+            breakClassName={"item"}
+            nextLabel=">"
+            nextClassName={"item next"}
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={windowSize.current[0] < 600 ? 3 : 5}
+            pageCount={pageCount}
+            pageClassName={"item pagination-page"}
+            previousLabel="<"
+            previousClassName={"item previous"}
+            renderOnZeroPageCount={null}
+          />
+        </div>
+
+        {/* {loading
           ? (<Loader />)
           : (
           <div className="app__container">
@@ -128,7 +191,7 @@ const App: React.FC = () => {
               nextLabel=">"
               nextClassName={"item next"}
               onPageChange={handlePageClick}
-              pageRangeDisplayed={5}
+              pageRangeDisplayed={windowSize.current[0] < 600 ? 3 : 5}
               pageCount={pageCount}
               pageClassName={"item pagination-page"}
               previousLabel="<"
@@ -137,7 +200,7 @@ const App: React.FC = () => {
             />
           </div>
           )
-        }
+        } */}
       </div>
     </div>
   );
